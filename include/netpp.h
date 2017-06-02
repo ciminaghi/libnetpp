f419d3650b8cc2a36ae338d03ec737a6c36d8bca
Davide Ciminaghi <ciminaghi@gnudd.com>
2017-06-02 18:21:56 +0200

    add libnetpp main header file
    Currently only contains logger related stuff

---
+++ include/netpp.h
@@ -0,0 +1,24 @@
+// libnet: simple C++ networking library for arduino (and maybe not
+// only arduino)
+// LGPL v2.1
+// Copyright Arduino S.r.l.
+// Author Davide Ciminaghi 2017
+
+// netpp main header
+#ifndef __netpp_h__
+#define __netpp_h__
+
+#include "logger.h"
+#include "event.h"
+
+namespace netpp {
+
+  static inline void fatal(const string& s, int code)
+  {
+    log_fatal << s << endl << flush;
+    *event_manager::get() << fatal_event::get_instance();
+  }
+
+};
+
+#endif // __netpp_h__
