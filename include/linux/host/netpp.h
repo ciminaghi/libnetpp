dc62a04a8b0c53c148cfaa2b1c1d990e5056092e
Davide Ciminaghi <ciminaghi@gnudd.com>
2017-06-04 07:25:44 +0200

    linux headers: introduce compatibility header

---
+++ include/linux/host/netpp.h
@@ -0,0 +1,10 @@
+// libnet: simple C++ networking library for arduino (and maybe not
+// only arduino)
+// LGPL v2.1
+// Copyright Arduino S.r.l.
+// Author Davide Ciminaghi 2017
+
+// linux/host/netpp.h: low level compat header file
+
+#include <unistd.h>
+
