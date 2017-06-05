7025bae2b75c84ed6215ab0315544c36d373502b
Davide Ciminaghi <ciminaghi@gnudd.com>
2017-06-05 13:27:55 +0200

    esp8266 headers: add compat header file

---
+++ include/esp8266/host/netpp.h
@@ -0,0 +1,9 @@
+// libnet: simple C++ networking library for arduino (and maybe not
+// only arduino)
+// LGPL v2.1
+// Copyright Arduino S.r.l.
+// Author Davide Ciminaghi 2017
+
+// esp8266/host/netpp.h: low level compat header file
+
+extern int write(int fd, const char *buf, int size);
