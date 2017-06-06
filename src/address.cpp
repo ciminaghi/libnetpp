0b9edd1f376c96ab48ef0c89b52a30e2f9339498
Davide Ciminaghi <ciminaghi@gnudd.com>
2017-06-06 13:58:21 +0200

    add declaration of address:unknown

---
+++ src/address.cpp
@@ -0,0 +1,13 @@
+// libnet: simple C++ networking library for arduino (and maybe not
+// only arduino)
+// LGPL v2.1
+// Copyright Arduino S.r.l.
+// Author Davide Ciminaghi 2017
+
+// address.cpp: implementation of addressing related classes
+
+#include "address.h"
+
+using namespace netpp;
+
+address address::unknown;
